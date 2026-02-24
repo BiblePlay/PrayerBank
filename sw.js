704cf92ce516759cd903e2ec7e6e876083e79d59
BiblePlay <merrymiri@gmail.com>
2026-02-24 23:35:57 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,36 +1,25 @@
-const CACHE_NAME = "prayer-bank-v2";
+const CACHE_NAME = 'prayerbank-v1';
 const ASSETS = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
 ];
-
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).catch(()=>{})
-  );
-  self.skipWaiting();
+self.addEventListener('install', (e)=> {
+  e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
 });
-
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
+self.addEventListener('activate', (e)=> {
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME?caches.delete(k):null))).then(()=>self.clients.claim()));
 });
-
-self.addEventListener("fetch", (event) => {
-  const req = event.request;
-  if (req.method !== "GET") return;
-  event.respondWith(
-    caches.match(req).then((cached) => cached || fetch(req).then((resp) => {
-      const copy = resp.clone();
-      caches.open(CACHE_NAME).then((cache) => cache.put(req, copy)).catch(()=>{});
-      return resp;
-    }).catch(()=>cached))
+self.addEventListener('fetch', (e)=> {
+  const req = e.request;
+  if(req.method!=='GET') return;
+  e.respondWith(
+    caches.match(req).then(res=> res || fetch(req).then(net=>{
+      const copy = net.clone();
+      caches.open(CACHE_NAME).then(c=>c.put(req, copy));
+      return net;
+    }).catch(()=>res))
   );
 });
